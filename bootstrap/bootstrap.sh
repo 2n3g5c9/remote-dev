65e5e37fd52b1ad420107122831f18228776d532
Marc Molina <marc.m@outlook.com>
2021-08-22 11:16:05 +0100

    Fixes permission issue in bootstrap script

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -14,7 +14,7 @@ apt-sources() {
     curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.gpg | sudo apt-key add -
     curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.list | sudo tee /etc/apt/sources.list.d/tailscale.list
     curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-    apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+    sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 }
 
 apt-upgrade() {

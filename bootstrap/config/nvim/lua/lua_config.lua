587b0c10d738e90033a2864592dce1fa9e19ef6a
Marc Molina <marc.m@outlook.com>
2021-08-30 11:12:11 +0000

    Fixes Hop hint_words mapping

--- bootstrap/config/nvim/lua/lua_config.lua
+++ bootstrap/config/nvim/lua/lua_config.lua
@@ -9,7 +9,7 @@ require'lualine'.setup{
 
 -- Configuration for Hop
 require'hop'.setup()
-vim.api.nvim_set_keymap('n', 'w', "<cmd>lua require'hop'.hint_words()<cr>", {})
+vim.api.nvim_set_keymap('n', '<space>w', "<cmd>lua require'hop'.hint_words()<cr>", {})
 vim.api.nvim_set_keymap('n', 'f', "<cmd>lua require'hop'.hint_char1()<cr>", {})
 vim.api.nvim_set_keymap('n', 's', "<cmd>lua require'hop'.hint_char2()<cr>", {})
 

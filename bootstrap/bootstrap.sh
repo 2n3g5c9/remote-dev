e7807f3984ff1067188eac63b9d4cecb1f4b078b
Marc Molina <marc.m@outlook.com>
2022-05-11 21:07:25 +0100

    Remove HashiCorp repository

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -8,8 +8,6 @@ apt-sources() {
     echo " ==> Adds additional APT sources"
     curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
     curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
-    curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-    sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 }
 
 apt-upgrade() {
